f85c235f340ce9b4391c15682016674697dc67ec
Sameer <sameerkumarsahu330@gmail.com>
2025-08-22 09:39:52 +0530

    feat(auth): implemented register, login and protected user route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { registerStudent, login } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/register", registerStudent);
 router.post("/login", login);
+
+router.get("/me", protect, (req, res) => {
+    res.json(req.user);
+})
 
 export default router;
